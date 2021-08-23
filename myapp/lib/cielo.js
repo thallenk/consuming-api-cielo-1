8b1123ba45952658a0ec45697f1f99b5d97d98f3
thallenk <thallen_k@hotmail.com>
2021-08-23 15:10:50 -0300

    Add the purchase capture

--- myapp/lib/cielo.js
+++ myapp/lib/cielo.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 class Cielo {
     static compra(params){ 
-        fetch('https://apisandbox.cieloecommerce.cielo.com.br/1/sales/', {
+        return fetch('https://apisandbox.cieloecommerce.cielo.com.br/1/sales/', {
                 method: 'post',
                 body:    JSON.stringify(params),
                 headers: { 
@@ -11,9 +11,20 @@ class Cielo {
                     'MerchantId': 'ab1d6d33-eb6d-421d-9913-fc5ece695df5',
                     'MerchantKey': 'HRUCJXXPZQYBAALQMRJFDBFVHVSBRSPPFHGWIEAC'},
             })
+            //Dessa forma, ainda está retornando um promise, por isso precisamos tratá-la no arquivo de compras
             .then(res => res.json())
-            .then(json => console.log(json));
     }
+    static captura(PaymentId){ 
+        return fetch(`https://apisandbox.cieloecommerce.cielo.com.br/1/sales/${PaymentId}/capture`, {
+                method: 'put',
+                headers: { 
+                    'Content-Type': 'application/json', 
+                    'MerchantId': 'ab1d6d33-eb6d-421d-9913-fc5ece695df5',
+                    'MerchantKey': 'HRUCJXXPZQYBAALQMRJFDBFVHVSBRSPPFHGWIEAC'},
+            })
+            //Dessa forma, ainda está retornando um promise, por isso precisamos tratá-la no arquivo de compras
+            .then(res => res.json())
+        }
 }
 
 module.exports = Cielo;
--- myapp/routes/compras.js
+++ myapp/routes/compras.js
@@ -4,12 +4,38 @@ var cielo = require('./../lib/cielo')
 
 /* Post criação de compra */
 router.post('/', function(req, res, next) {
-  res.send(cielo.compra(req.body));
+
+  cielo.compra(req.body).then((result)=>{
+
+    // res.send(result)
+  cielo.captura(result.Payment.PaymentId).then((result)=>{
+    if(result.Status == 2){
+      res.status(201).send({
+        "Status": "Sucesso",
+        "Message": "Compra realizada com sucesso"
+      })
+    }
+    else{
+      res.status(402).send({
+        "Status": "Falhou",
+        "Message": "Compra não realizada. Problema na cobrança do cartão de crédito."
+      })
+    }
+    
+  })
+  .catch((err)=>{
+    console.error(err)
+  })
+  })
+
+
+  
 });
 
 /* GET status de compra */
 router.get('/:compra_id/status', function(req, res, next) {
   res.send('Rodando status..');
+
 });
 
 module.exports = router;
